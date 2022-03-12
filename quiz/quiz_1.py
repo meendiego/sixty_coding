f0dc05023094189ae3b6db0242d7a0c6212aab80
Chloe Choi <bounce2827@gmail.com>
2022-03-12 23:53:46 +0900

    220320 quiz

---
+++ quiz/quiz_1.py
@@ -0,0 +1,99 @@
+def exercise_1():
+    pass
+
+
+def exercise_2():
+    wage = 5  # 시급 (1시간에 5달러)
+    exchange_rate = 1142.16  # 환율 (1달러에 1142.16원)
+
+    # "1시간에 5달러 벌었습니다." 출력
+    print("{}시간에 {}{} 벌었습니다.".format(1, wage * 1, "달러"))
+
+    # "5시간에 25달러 벌었습니다." 출력
+    print()  # 코드를 채워 넣으세요.
+
+    # "1시간에 5710.8원 벌었습니다." 출력
+    print()  # 코드를 채워 넣으세요.
+
+    # "5시간에 28554.0원 벌었습니다." 출력
+    print()  # 코드를 채워 넣으세요.
+
+
+def exercise_3():
+    def is_evenly_divisible(number):
+        # 코드를 작성하세요.
+        pass
+
+    # 테스트
+    print(is_evenly_divisible(3))
+    print(is_evenly_divisible(7))
+    print(is_evenly_divisible(8))
+    print(is_evenly_divisible(218))
+    print(is_evenly_divisible(317))
+
+
+def exercise_4():
+    def calculate_change(payment, cost):
+        # 코드를 작성하세요.
+        pass
+
+    # 테스트
+    calculate_change(100000, 33000)
+    print()
+    calculate_change(500000, 378000)
+
+
+def exercise_5():
+    i = 100
+
+    # 코드를 입력하세요.
+
+
+def exercise_6():
+    pass
+
+
+def exercise_7():
+    def print_grade(midterm_score, final_score):
+        total = midterm_score + final_score
+        # 코드를 쓰세요.
+
+    # 테스트
+    print_grade(40, 45)
+    print_grade(20, 35)
+    print_grade(30, 32)
+    print_grade(50, 45)
+
+
+def exercise_8():
+    pass
+
+
+def exercise_9():
+    pass
+
+
+def exercise_10():
+    pass
+
+
+if __name__ == '__main__':
+    exercise_1()
+    print('--------------------------------------------------------\n')
+    exercise_2()
+    print('--------------------------------------------------------\n')
+    exercise_3()
+    print('--------------------------------------------------------\n')
+    exercise_4()
+    print('--------------------------------------------------------\n')
+    exercise_5()
+    print('--------------------------------------------------------\n')
+    exercise_6()
+    print('--------------------------------------------------------\n')
+    exercise_7()
+    print('--------------------------------------------------------\n')
+    exercise_8()
+    print('--------------------------------------------------------\n')
+    exercise_9()
+    print('--------------------------------------------------------\n')
+    exercise_10()
