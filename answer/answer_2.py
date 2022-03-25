a3c538153b133117460998128e50368a79c22175
Chloe Choi <bounce2827@gmail.com>
2022-03-25 20:48:02 +0900

    quiz2 answer

--- answer/answer_2.py
+++ answer/answer_2.py
@@ -153,11 +153,13 @@ def exercise_9():
 
 def exercise_10():
     import pandas as pd
+    import matplotlib.pyplot as plt
 
     df = pd.read_csv('../data/gdp.csv', index_col=0)
 
     # 코드를 작성하세요.
     df[['Korea_Rep', 'United_States', 'United_Kingdom', 'Germany', 'China', 'Japan']].plot()
+    plt.show()
 
 
 if __name__ == '__main__':
